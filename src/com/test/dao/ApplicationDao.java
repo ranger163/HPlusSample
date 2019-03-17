d197102fbdab9bd133313f0ad7d627a53e1c5cf8
Nassar163 <ahmed.nassar163@gmail.com>
2019-03-17 19:54:22 +0200

    Querying the database

--- src/com/test/dao/ApplicationDao.java
+++ src/com/test/dao/ApplicationDao.java
@@ -2,12 +2,39 @@ package com.test.dao;
 
 import com.test.beans.Product;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApplicationDao {
 
-    public List<Product>searchProducts(String searchString){
+    public List<Product> searchProducts(String searchString) {
 
+        Product product = null;
+        List<Product> products = new ArrayList<>();
+        try {
+            Connection connection = DBConnection.getConnectionDataBase();
+
+            String sql = "SELECT * FROM PRODUCTS WHERE product_name like '%" + searchString + "%'";
+
+            Statement statement = connection.createStatement();
+
+            ResultSet set = statement.executeQuery(sql);
+
+            while (set.next()) {
+                product = new Product();
+                product.setProductId(set.getInt("product_id"));
+                product.setProductName(set.getString("product_name"));
+                product.setProductImgPath(set.getString("image_path"));
+                products.add(product);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return products;
     }
 
 }
