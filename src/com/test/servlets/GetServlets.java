96dffd4fc51fafe66a6a916ccbc0ce1c769464d2
Nassar163 <ahmed.nassar163@gmail.com>
2019-03-19 19:10:24 +0200

    Understanding ServletConfig

--- src/com/test/servlets/GetServlets.java
+++ src/com/test/servlets/GetServlets.java
@@ -1,6 +1,8 @@
 package com.test.servlets;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,7 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet("/getServlets")
+@WebServlet(urlPatterns = "/getServlets",
+        initParams = @WebInitParam(name = "URL", value = "http://www.weatherservice.com"))
 public class GetServlets extends HttpServlet {
 
     private PrintWriter writer;
@@ -16,15 +19,14 @@ public class GetServlets extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         initialize(resp);
+
+        ServletConfig config = getServletConfig();
+        System.out.println(config.getInitParameter("URL"));
+
         String htmlResponse = "<html><h1>Welcome to servlets.</h1></html>";
         writer.write(htmlResponse);
         String name = req.getParameter("name");
         writer.write(name);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        initialize(resp);
     }
 
     private void initialize(HttpServletResponse resp) throws IOException {
